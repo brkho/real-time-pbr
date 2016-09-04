3f45075166bdcc8c62660c46c587bb9f9b9f8572
Brian Ho <brian@brkho.com>
2016-09-04 03:43:44 -0700

    Add EBO support for instanced rendering.

--- src/main.cc
+++ src/main.cc
@@ -115,17 +115,26 @@ int main(int /* argc */, char* /* argv */[]) {
   glfwSetKeyCallback(window, key_callback);
 
   GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f
+    -0.5f, -0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+  };
+  GLuint indices[] = {
+    0, 1, 3,
+    1, 2, 3,
   };
   GLuint vao;
+  GLuint vbo;
+  GLuint ebo;
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
-  GLuint vbo;
   glGenBuffers(1, &vbo);
+  glGenBuffers(1, &ebo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
   glEnableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -140,7 +149,7 @@ int main(int /* argc */, char* /* argv */[]) {
 
     glUseProgram(program); 
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glfwSwapBuffers(window);
   }
 
